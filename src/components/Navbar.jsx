6c39380c46876228218efe11a88f581f865a3344
yuusufxb <youssefmouha98@gmail.com>
2026-09-25 02:25:52 +0100

    handling the contact page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom"
-import "../css/navbar.css"
+import "./css/navbar.css"
 
 export default  function Navbar(){
     
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,76 @@
-
+import { useState } from "react"
+import "./css/contact.css"
 
 export default function Contact(){
+    const [contactForm,SetcontactForm] = useState({
+        name :"" ,
+        email:"" ,
+        message : "" ,
+    });
+    const handleChange = (e) => {
+        const  {name,value} = e.target  ;
+        SetcontactForm((prev)=>({
+            ...prev ,
+            [name]:value ,
+        }))
+    }; 
+
+    const handleSubmit = async (e)=>{
+        e.preventDefault() ;
+        console.log(contactForm);
+    }
+
+
+
     return(
         <>
-        <h1>Contact</h1>
+        <div className="contact-container">
+            <h1>Contact</h1>
+            <p className="subtitle">Have a question? Send me a message.</p>
+
+            <form onSubmit={handleSubmit}className="contact-form">
+            <div className="input-group">
+                <label htmlFor="name">Name</label>
+                <input
+                value={contactForm.name}
+                name="name"
+                id="name"
+                type="text"
+                required
+                placeholder="Example"
+                onChange={handleChange}
+                />
+            </div>
+
+            <div className="input-group">
+                <label htmlFor="email">Email</label>
+                <input
+                value={contactForm.email}
+                name="email"
+                id="email"
+                type="email"
+                required
+                placeholder="example@example.com"
+                onChange={handleChange}
+                />
+            </div>
+
+            <div className="input-group">
+                <label htmlFor="content">Message</label>
+                <textarea
+                value={contactForm.message}
+                name="message"
+                id="content"
+                required
+                placeholder="Write your message..."
+                onChange={handleChange}
+                />
+            </div>
+
+            <button type="submit">Send Message</button>
+            </form>
+        </div>
         </>
+
     )
 }
